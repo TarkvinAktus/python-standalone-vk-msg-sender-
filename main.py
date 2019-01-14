fb93be89f92ee6679dc60c571f21f362dc45de74
unknown <aleshin1196@gmail.com>
2019-01-14 22:27:35 +0300

    list of groups

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PyQt5 import QtWidgets
 import design  # Это наш конвертированный файл дизайна
 import vk_api
 import random
+import time
 
 class Second(QtWidgets.QMainWindow, design.Ui_MainWindow):
 
@@ -21,13 +22,13 @@ class Main(QtWidgets.QMainWindow, design.Ui_MainWindow):
 
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
 
-        #self.pushButton.clicked.connect(self.sendMsg)
+        self.pushButton.clicked.connect(self.sendMsg)
 
-        self.pushButton.clicked.connect(self.on_pushButton_clicked)
-        self.dialog = Second(self)
+        #self.pushButton.clicked.connect(self.on_pushButton_clicked)
+        #self.dialog = Second(self)
 
-    def on_pushButton_clicked(self):
-        self.dialog.show()
+    #def on_pushButton_clicked(self):
+       # self.dialog.show()
     
     def sendMsg(self): 
         rand_id = 0
@@ -35,13 +36,74 @@ class Main(QtWidgets.QMainWindow, design.Ui_MainWindow):
         vk.auth() 
         vk_get_api = vk.get_api() 
 
-        vk_get_api.messages.send( 
-            peer_id=, 
-            message='', 
-            random_id = rand_id
-        )
+        allConversations = vk_get_api.messages.getConversations()
 
-        rand_id = rand_id + 1
+        #API has limit for requests equals to 20 
+        #so we calculate num of iterations and check all dialogs what we need 
+        text = str(self.textEdit.toPlainText())
+
+        numOfConv = allConversations["count"]
+
+        lim = numOfConv//20 + 1
+        j = 0
+        i = 0
+        msgForce = 1
+        randIdForMsg = 0
+
+        print(numOfConv)
+        print(j)
+
+        counter=0
+        prev_fail = 0
+
+
+        myOffset=0
+        while j < lim:
+            #time.sleep(3)
+            allConversations = vk_get_api.messages.getConversations(offset=myOffset)
+            i = 0
+
+            while i < 20:
+                try:
+                    if allConversations["items"][i]["conversation"]["peer"]["type"]=="chat" and allConversations["items"][i]["conversation"]["chat_settings"]["title"][0]!="Э":
+                        print(allConversations["items"][i]["conversation"]["chat_settings"]["title"], end = " --- ")        
+                        print(allConversations["items"][i]["conversation"]["peer"]["local_id"])
+                        #time.sleep(1)
+                        id = 2000000000+(allConversations["items"][i]["conversation"]["peer"]["local_id"])
+                        #This is catch for message.send() limits of api
+                        #if msg is not send we wait for 1 second and try again
+                        while msgForce==1:
+                            try:
+                                #vk_get_api.messages.send(random_id=rand_id,peer_id=id,message=text, random_id = randIdForMsg)
+                                #rand_id = rand_id + 1
+                                msgForce = 0
+                                randIdForMsg = randIdForMsg + 1
+                            except:
+                                time.sleep(1)
+                                msgForce = 1    
+
+                        counter = counter + 1
+                        prev_fail = 0
+                except:
+                    #This segment starts when something goes wrong
+                    if prev_fail == 0:
+                        print("---")
+                    prev_fail = 1
+                i = i + 1
+            myOffset = myOffset + 20
+            j = j + 1
+            
+        print("Найдено "+str(counter))
+
+
+        #single msg
+        #vk_get_api.messages.send( 
+        #    peer_id=35109961, 
+        #    message=str(self.textEdit.toPlainText()), 
+        #    random_id = rand_id
+        #)
+
+        #rand_id = rand_id + 1
 
 def main():
     app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
